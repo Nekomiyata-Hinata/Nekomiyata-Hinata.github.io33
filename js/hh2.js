d111c66e459b504bf75d972c25ce4002ca035bf7
Hinata <47328764+Nekomiyata-Hinata@users.noreply.github.com>
2019-09-05 05:56:20 -0500

    Update hh2.js

--- js/hh2.js
+++ js/hh2.js
@@ -1,1 +1,6 @@
-var loopy()   function loopy() {   var sWord =""  while (sWord != "123") {//设置密码sWord = prompt("输入正确密码才能登陆!")   }   alert("欢迎访问")   }
+var loopy()
+function loopy() {
+  var sWord =""
+  while (sWord != "123") {//设置密码sWord = prompt("输入正确密码才能登陆!")   }
+    alert("欢迎访问")
+  }
