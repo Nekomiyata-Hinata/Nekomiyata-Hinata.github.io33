737a8707127dc910ea9547b2e02d19148575e5ef
Hinata <47328764+Nekomiyata-Hinata@users.noreply.github.com>
2019-09-05 06:07:07 -0500

    Create hh3.js

---
+++ js/hh3.js
@@ -0,0 +1,17 @@
+<script languange="JavaScript">
+        var password=""
+        password=prompt('请输入密码(本网站需输入密码才可以进入):','');
+        function custom_close()
+        {
+            window.opener=null;
+            window.open('','_self');
+            window.close();        
+        }   
+        if  (password !='123456')
+            {alert("密码不正确，无法进入本站！！！")
+            
+            custom_close();            
+            alert("即将打开百度")//加这一段是为了避免有些浏览器不支持关闭浏览器的js ,所以请求跳转到一个新页面！
+            window.location.href='http://www.baidu.com';
+            }
+</script>
