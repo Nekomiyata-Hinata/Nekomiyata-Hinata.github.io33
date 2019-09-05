82a331177fb4d1e552c3731b249f317489193f19
Hinata <47328764+Nekomiyata-Hinata@users.noreply.github.com>
2019-09-05 06:08:44 -0500

    Update hh3.js

--- js/hh3.js
+++ js/hh3.js
@@ -1,4 +1,3 @@
-<script languange="JavaScript">
         var password=""
         password=prompt('请输入密码(本网站需输入密码才可以进入):','');
         function custom_close()
@@ -13,5 +12,4 @@
             custom_close();            
             alert("即将打开百度")//加这一段是为了避免有些浏览器不支持关闭浏览器的js ,所以请求跳转到一个新页面！
             window.location.href='http://www.baidu.com';
-            }
-</script>
+   }
