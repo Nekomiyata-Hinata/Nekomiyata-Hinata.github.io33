d3374f4b91edfa4a4fa769a185dfd5ed3108b12d
Hinata <47328764+Nekomiyata-Hinata@users.noreply.github.com>
2019-09-05 06:03:54 -0500

    Update hh2.js

--- js/hh2.js
+++ js/hh2.js
@@ -1,1 +1,19 @@
-function password() {   var testV = 1;   var pass1 = prompt('请输入密码:','');   while (testV < 3) {   if (!pass1)    history.go(-1);   if (pass1 == "123") {//设置密码alert('口令正确，进行跳转');   window.location.href="https://www.kxdao.net";//添加你要跳转的页面break;   }    testV+=1;   var pass1 =    prompt('密码错误','');   }   if (pass1!="password" & testV ==3)    history.go(-1);   return " ";   }    document.write(password());   
+function password() {   
+var testV = 1;   
+var pass1 = prompt('请输入密码','');   
+while (testV < 3) {   
+if (!pass1)   
+history.go(-1);   
+if (pass1 == "123") {//初始密码123   
+alert('密码正确');   
+break;   
+}   
+testV+=1;   
+var pass1 =   
+prompt('密码错误!请重新输入:');   
+}   
+if (pass1!="password" & testV ==3)   
+history.go(-1);   
+return " ";   
+}   
+document.write(password());   
